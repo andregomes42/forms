982082a6f2c305adfb5e985db8994305c128cf38
costelinha <andregomesngr@gmail.com>
2020-10-01 14:53:52 -0500

    Refactoring routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'answers/index'
-      get 'answers/show'
-      get 'answers/create'
-      get 'answers/destroy'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'questions/create'
-      get 'questions/update'
-      get 'questions/destroy'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'forms/index'
-      get 'forms/show'
-      get 'forms/update'
-      get 'forms/create'
-      get 'forms/destroy'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
+      resources :forms
+      resources :questions, only: [:create, :update, :destroy]
+      resources :answers, only: [:index, :show, :create, :destroy]
     end
   end
 end
